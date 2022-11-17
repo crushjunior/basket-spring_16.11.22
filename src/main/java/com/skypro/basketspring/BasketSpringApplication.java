17397303f48458cc23c5392a71ca57cf31663e3b
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-11-17 19:27:29 +0300

    Initial commit

---
+++ src/main/java/com/skypro/basketspring/BasketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.basketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/basketspring/BasketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.basketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
